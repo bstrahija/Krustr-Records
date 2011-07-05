c9b8e8db06a240028ec291cb6b5e54e7a521ee3c
Boris Strahija <boris@creolab.hr>
2011-07-06 00:09:49 +0200

    Some changes to themes

---
+++ addons/themes/crunch/config/assets.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Combine / Minify / Less
+|--------------------------------------------------------------------------
+|
+| Flags whether files should be combined or minified, and css parsed with less.
+|
+*/
+
+$config['assets']['env']         = 'production'; // Environment (if it's set to 'dev', no processing will be done)
+$config['assets']['combine']     = true;
+$config['assets']['combine_js']  = true;
+$config['assets']['combine_css'] = true;
+$config['assets']['minify_js']   = false;
+$config['assets']['minify_css']  = true;
+$config['assets']['less_css']    = true;
+
+/*
+|--------------------------------------------------------------------------
+| Cache
+|--------------------------------------------------------------------------
+|
+| Define if the cache folder should be cleared when generating new cache files
+| 
+*/
+
+$config['assets']['auto_clear_cache']     = true;
+$config['assets']['auto_clear_css_cache'] = false;
+$config['assets']['auto_clear_js_cache']  = false;
+
+/*
+|--------------------------------------------------------------------------
+| Default paths and directories, tags
+|--------------------------------------------------------------------------
+|
+| Default directories containing the assets
+| Option to use HTML5 tags
+|
+*/
+
+$config['assets']['assets_dir']    = ADDONPATH.'themes/crunch/assets';
+$config['assets']['js_dir']        = 'js';
+$config['assets']['css_dir']       = 'css';
+$config['assets']['cache_dir']     = 'cache';
+$config['assets']['html5']         = true;
+$config['assets']['auto_protocol'] = true;
